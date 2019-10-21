a69be41b073f0ec60f88921b0e63270d8d710934
Leo <leonardom1808@gmail.com>
2019-10-21 14:30:53 -0300

    init

---
+++ app/src/main/java/br/edu/al/leonardomm4/logapp/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.al.leonardomm4.logapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

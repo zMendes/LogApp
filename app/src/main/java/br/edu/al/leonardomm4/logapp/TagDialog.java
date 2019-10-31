299ee4e45b8e71830827e1b802a3111a7e51eaa4
Leo <leonardom1808@gmail.com>
2019-10-31 11:21:35 -0300

    Dialog mais bonito

--- app/src/main/java/br/edu/al/leonardomm4/logapp/TagDialog.java
+++ app/src/main/java/br/edu/al/leonardomm4/logapp/TagDialog.java
@@ -2,37 +2,35 @@ package br.edu.al.leonardomm4.logapp;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatDialogFragment;
 
-public class TagDialog extends AppCompatDialogFragment {
+import java.util.Objects;
 
+public class TagDialog extends AppCompatDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        LayoutInflater inflater = getActivity().getLayoutInflater();
+        LayoutInflater inflater = Objects.requireNonNull(getActivity()).getLayoutInflater();
 
         View view = inflater.inflate(R.layout.layout_tag, null);
 
+        String[] tags = new String[]{"Pains", "Gains", "Explicação", "Teste"};
+
         builder.setView(view)
                 .setTitle("Tags")
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                }).setPositiveButton("ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-
-            }
+                .setNegativeButton("Cancel", (dialogInterface, i) -> {
+                }).setPositiveButton("ok", (dialogInterface, i) -> {
+            Toast.makeText(getActivity(), "Tag adicionada com sucesso", Toast.LENGTH_SHORT).show();
         });
+        builder.setSingleChoiceItems(tags, 0, null);
         return builder.create();
     }
+
 }
